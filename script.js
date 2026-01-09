9377cbebe89b72dfe1b86a3b84c4deba73e630da
sriranjanijp <sriranjani.jp@gmail.com>
2026-01-09 16:59:56 +0530

    final final final

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function chk1() {
     let nam = document.getElementById('nam').value;
+    let pass = document.getElementById('pass').value;
 
-    if (nam.length > 0 && nam[0] === nam[0].toUpperCase()) {
+    if (pass === "admin" && nam === "admin") {
         window.location.href = "step2.html";
     } else {
         document.getElementById('msg').innerText = "Wrong entry!";
-        document.getElementById('hint').innerText = "Hint: Names must start with capital letters"
+        document.getElementById('hint').innerText = "Hint: only __ access"
     }
 }
 
@@ -25,7 +26,7 @@ function chk2() {
     }
 
     if (parseInt(val) === sum) {
-        window.location.href = "step3.html";
+        window.location.href = "step4.html";
     } else {
         document.getElementById('hint').innerText = "Hint: 08/01/2026 = 0+8+0+1+2+0+2+6 = 19"
         document.getElementById('msg').innerText = "Wrong sum entered!";
@@ -34,17 +35,39 @@ function chk2() {
 
 function chk3() {
     let lyr = document.getElementById('lyr').value.toLowerCase();
-    let ans = "ready a room at the hotel california"; 
+    let ans = "hotel california"; 
 
     if (lyr === ans) {
-        window.location.href = "page2.html";
+        window.location.href = "final.html";
     } else {
-        document.getElementById('msg').innerText = "Wrong lyrics! Try again.";
-        document.getElementById('hint').innerText = "Hint: Starts with \"ready a ...?\""
+        document.getElementById('msg').innerText = "Wrong song! Try again.";
+        document.getElementById('hint').innerText = "Hint: Starts with \"Hotel ...?\""
     }
 }
 
 function play() {
     let aud = document.getElementById('aud');
     aud.play();
+}
+
+window.onload = captcha;
+var result;
+
+function captcha(){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    result = '';
+    for (let i = 0; i < 5; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    
+    document.getElementById('captcha').innerText = result;
+}
+
+function captchaCheck(){
+    let inp = document.getElementById('inp').value;
+    if (inp === result) {
+        window.location.href = "step3.html";
+    } else {
+        document.getElementById('msg').innerText = "Wrong captcha! Try again."+result;
+    }
 }
